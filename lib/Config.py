c2813200a7c61a4ae2f153b16509e3b2fb6c3771
Jdoing <703627622@qq.com>
2015-04-16 22:22:37 +0800

    update config.py

--- lib/Config.py
+++ lib/Config.py
@@ -6,27 +6,42 @@ log = logging.getLogger(__name__)
 
 class ConfigError(Exception):
     pass
-    
+
 class Server(object):
-    def __init__(self, ip, username, password, port):
-        self.ip = ip
-        self.username = username
-        self.password = password
-        self.port = port
+    def __init__(self):
+        self.ip = ''
+        self.username = ''
+        self.password = ''
+        self.port = ''
 
 class SysConfig(object):
-    configParser = ConfigParser.ConfigParser()
+    #configParser = ConfigParser.ConfigParser()
     
-    def __init__(self):
+    def __init__(self, filename):
+        self.configname = filename
         self.servers = []
-        
-    @staticmethod
-    def parseConfig(filename):
+        self.parser = ConfigParser.ConfigParser()
+
+    def parseConfig(self):
         if not Path.isfile():
             sys.exit('SysConfig file name is not a real file')
+        self.servers = self.parser_from_file()
         
-   
-
+    def parser_from_file(self):
+        self.parser.read(self.configname)
+        sections = self.parser.sections()
+        
+        ips = []
+        for section in sections:
+            if section == 'servers':
+                ips = self.get_server_ip(section)
+            elif section == 'global':
+                pass
+            elif section
+                
+        
+    def get_server_ips(self, section):
+        return map(lambda t : t[1], self.parser.options(section))
 
 
 class CaseConfig(object):
--- test.py
+++ test.py
@@ -3,15 +3,8 @@ import os, sys
 from lib import GlobalSet
 #from lib.TestRunner.Runner import Runner
 
-class Case(object):
-    def __init__(self, name, time=0, errorType=None, errorMessage=None, status='pass', params=''):
-        self.name = name
-        self.time = time
-        self.errorType = errorType
-        self.errorMessage = errorMessage
-        self.status = status
-        self.params = params
+db = [('db_host', '127.0.0.1'), ('db_port', '22'), ('db_user', 'root'), ('db_pass', 'rootroot')]
 
-case = Case("t001")
-case.status = 'fail'
-print case.__dict__
+print map(lambda t : t[1], db)
+
+print zip('abc', '123')
